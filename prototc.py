9129224146b528279cd276121f3d61478346153f
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-04-18 18:28:46 -0400

    Created stores

--- prototc.py
+++ prototc.py
@@ -1,7 +1,60 @@
 from scssdk_dataclasses import Telemetry, TYPE_MACROS_BY_ID, SCS_TELEMETRY_trailers_count, load
 
 
+VALUE_STORAGE_STRUCT_NAME: str = 'value_storage'
+VALUE_ARRAY_STORAGE_STRUCT_NAME: str = 'value_array_storage'
+TRAILER_DATA_STRUCT_NAME: str = 'trailer_data'
+TRAILER_DATA_INSTANCE_NAME: str = 'trailers'
+STORE_STRUCT_NAME: str = 'game_data_store'
+STORE_INSTANCE_NAME: str = 'game_data'
 STORE_FUNCTION_NAME: str = 'store'
+
+
+def containers() -> str:
+    return (
+        'template <typename T>\n'
+        f'struct {VALUE_STORAGE_STRUCT_NAME} {{\n'
+        '\tT value;\n'
+        '\tbool initialized = false;\n'
+        '};\n'
+        '\n'
+        'template <typename T, size_t max_count>\n'
+        f'struct {VALUE_ARRAY_STORAGE_STRUCT_NAME} {{\n'
+        '\tstd::array<T, max_count> values;\n'
+        '\tbool initialized = false;\n'
+        '};\n'
+        '\n'
+    )
+
+
+def store_struct(telemetries: list[Telemetry]) -> str:
+    out: str = f'struct {TRAILER_DATA_STRUCT_NAME} {{\n'
+    for trailer_telemetry in filter(lambda t: t.is_trailer_channel, telemetries):
+        if trailer_telemetry.indexed:
+            container_type: str = VALUE_ARRAY_STORAGE_STRUCT_NAME
+            template_args: str = f'{trailer_telemetry.scs_type}, {trailer_telemetry.max_count}'
+        else:
+            container_type: str = VALUE_STORAGE_STRUCT_NAME
+            template_args: str = trailer_telemetry.scs_type
+        out += f'\t{container_type}<{template_args}> {trailer_telemetry.simple_name};\n'
+
+    out += (
+        '};\n'
+        '\n'
+        f'struct {STORE_STRUCT_NAME}{{\n'
+    )
+
+    for telemetry in filter(lambda t: not t.is_trailer_channel and not t.is_event, telemetries):
+        if telemetry.indexed:
+            container_type: str = VALUE_ARRAY_STORAGE_STRUCT_NAME
+            template_args: str = f'{telemetry.scs_type}, {telemetry.max_count}'
+        else:
+            container_type: str = VALUE_STORAGE_STRUCT_NAME
+            template_args: str = telemetry.scs_type
+        out += f'\t{container_type}<{template_args}> {telemetry.simple_name};\n'
+    out += f'\t{TRAILER_DATA_STRUCT_NAME} {TRAILER_DATA_INSTANCE_NAME};\n'
+    out += '};\n\n'
+    return out
 
 
 def registrations_for(telemetries: list[Telemetry], trailer_count: int = SCS_TELEMETRY_trailers_count, tabs: int = 1) -> str:
@@ -14,7 +67,6 @@ def registrations_for(telemetries: list[Telemetry], trailer_count: int = SCS_TEL
         expansion: str = telemetry.expansion
         type_macro: str = TYPE_MACROS_BY_ID[telemetry.scs_type_id]
         callback: str = f'{STORE_FUNCTION_NAME}<{telemetry.scs_type}, {telemetry.max_count}>' if telemetry.indexed else f'store<{telemetry.scs_type}>'
-        context: str = 'nullptr'
 
         if telemetry.is_trailer_channel:
             if telemetry.indexed:
@@ -24,7 +76,7 @@ def registrations_for(telemetries: list[Telemetry], trailer_count: int = SCS_TEL
                     f"{tab_str}for (scs_u32_t t = 0; t < {trailer_count}; t++) {{\n"
                     f"{tab_str}\t{telemetry.macro}_trailer_index_char = '0' + t;\n"
                     f'{tab_str}\tfor(scs_u32_t i = 0; i < {telemetry.max_count}; i++) {{\n'
-                    f'{tab_str}\t\t{function}({telemetry.macro}_expansion, i, {type_macro}, SCS_TELEMETRY_CHANNEL_FLAG_none, {callback}, {context});\n'
+                    f'{tab_str}\t\t{function}({telemetry.macro}_expansion, i, {type_macro}, SCS_TELEMETRY_CHANNEL_FLAG_none, {callback}, &{STORE_INSTANCE_NAME}.{TRAILER_DATA_INSTANCE_NAME}[t].{telemetry.simple_name});\n'
                     f"{tab_str}\t}}\n"
                     f"{tab_str}}}\n"
                 )
@@ -34,18 +86,18 @@ def registrations_for(telemetries: list[Telemetry], trailer_count: int = SCS_TEL
                     f'{tab_str}char& {telemetry.macro}_trailer_index_char = {telemetry.macro}_expansion[8];\n'
                     f"{tab_str}for (scs_u32_t t = 0; t < {trailer_count}; t++) {{\n"
                     f"{tab_str}\t{telemetry.macro}_trailer_index_char = '0' + t;\n"
-                    f'{tab_str}\t{function}({telemetry.macro}_expansion, SCS_U32_NIL, {type_macro}, SCS_TELEMETRY_CHANNEL_FLAG_none, {callback}, {context});\n'
+                    f'{tab_str}\t{function}({telemetry.macro}_expansion, SCS_U32_NIL, {type_macro}, SCS_TELEMETRY_CHANNEL_FLAG_none, {callback}, &{STORE_INSTANCE_NAME}.{TRAILER_DATA_INSTANCE_NAME}[t].{telemetry.simple_name});\n'
                     f"{tab_str}}}\n"
                 )
         else:
             if telemetry.indexed:
                 out = (
                     f"{tab_str}for (scs_u32_t i = 0; i < {telemetry.max_count}; i++) {{\n"
-                    f"{tab_str}\t{function}({telemetry.macro}, i, {type_macro}, SCS_TELEMETRY_CHANNEL_FLAG_none, {callback}, {context});\n"
+                    f"{tab_str}\t{function}({telemetry.macro}, i, {type_macro}, SCS_TELEMETRY_CHANNEL_FLAG_none, {callback}, &{STORE_INSTANCE_NAME}.{telemetry.simple_name});\n"
                     f"{tab_str}}}\n"
                 )
             else:
-                out = f"{tab_str}{function}({telemetry.macro}, SCS_U32_NIL, {type_macro}, SCS_TELEMETRY_CHANNEL_FLAG_none, {callback}, {context});\n"
+                out = f"{tab_str}{function}({telemetry.macro}, SCS_U32_NIL, {type_macro}, SCS_TELEMETRY_CHANNEL_FLAG_none, {callback}, &{STORE_INSTANCE_NAME}.{telemetry.simple_name});\n"
 
         yield out
 
@@ -60,7 +112,9 @@ def main() -> None:
         for registration in registrations_for(telemetries):
             f.write(registration)
         f.write('}\n')
-
+    with open('store.gitignore.cpp', 'w', encoding='utf-8') as f:
+        f.write(containers())
+        f.write(store_struct(telemetries))
 
 
 if __name__ == '__main__':
