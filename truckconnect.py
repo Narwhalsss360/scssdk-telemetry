b0ef11cd584d2106ed7c83e02751466ae19b7f40
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-06-09 12:50:11 -0400

    event_info_member_offset_of

--- truckconnect.py
+++ truckconnect.py
@@ -679,6 +679,30 @@ def offset_of_latest_function(telemetries: list[Telemetry], tabcount: int = 0) -
     return out
 
 
+def event_info_member_offset_of_function(telemetries: list[Telemetry], tabcount: int = 0) -> str:
+    tabstr: str = TAB_CHARS * tabcount
+    out: str = (
+        f"{tabstr}constexpr const size_t event_info_member_offset_of(const {TELEMETRY_ID_ENUM_TYPE_NAME}& id, const char* const member) {{\n"
+        f"{tabstr}{TAB_CHARS}switch (id) {{\n"
+    )
+
+    for i, telemetry in enumerate(filter(lambda t: t.is_event_info, telemetries)):
+        out += (
+            f"{tabstr}{TAB_CHARS * 2}case {telemetry.qualified_id}:\n"
+            f"{tabstr}{TAB_CHARS * 3}return\n"
+        )
+        for attribute in telemetry.as_event_info.attributes:
+            out += f"{tabstr}{TAB_CHARS * 4}streq(member, \"{attribute.simple_name}\") ? offsetof({name(telemetry)}::storage_type, {attribute.simple_name}) :\n"
+        out += f"{tabstr}{TAB_CHARS * 4}INVALID_OFFSET;\n"
+
+    out += (
+        f"{tabstr}{TAB_CHARS * 2}default: return INVALID_OFFSET;\n"
+        f"{tabstr}{TAB_CHARS}}}\n"
+        f"{tabstr}}}\n"
+    )
+    return out
+
+
 PAUSED_CUSTOM_CHANNEL: Channel = Channel(
     "channel_paused", "", "bool", False, "channel_paused", False, 1
 )
@@ -721,6 +745,7 @@ def main() -> None:
         f.write(f"{name_of_function(telemetries)}\n")
         f.write(f"{size_of_function(telemetries)}\n")
         f.write(f"{offset_of_latest_function(telemetries)}\n")
+        f.write(f"{event_info_member_offset_of_function(telemetries)}\n")
 
 
 if __name__ == "__main__":
