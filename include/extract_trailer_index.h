eb3d7816f0db4b794f2ed6651ddb86b260e06e40
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-06-11 18:02:09 -0400

    extract_trailer_index returns uint32_t

--- include/extract_trailer_index.h
+++ include/extract_trailer_index.h
@@ -1,4 +1,4 @@
-constexpr const size_t extract_trailer_index(const char* const cstr, const bool reversing = true, size_t count = 0) {
+constexpr const uint32_t extract_trailer_index(const char* const cstr, const bool reversing = true, size_t count = 0) {
 	return (
 		reversing ?
 			extract_trailer_index(cstr + 1, *(cstr + 1) != '\0', count + 1) :
