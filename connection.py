faab7e23b9459133aa9e50740df1b8671abbba6f
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-08-08 11:59:51 -0400

    Added requests for telemetries

--- connection.py
+++ connection.py
@@ -3,13 +3,25 @@ from enum import Enum
 from socket import socket, AddressFamily, SocketKind, IPPROTO_TCP
 from dataclasses import dataclass
 from nstreamcom import Collector, encode_with_size
-from truckconnect import INVALID_TELEMETRY_ID, Version, VERSION
+from truckconnect import INVALID_TELEMETRY_ID, Telemetry, Version, VERSION, telemetry
+from telemetry_id import TelemetryID
+from time import sleep, time
+from struct import unpack_from
 
 
 @dataclass
 class TrailerIndexOrCount:
     is_count: bool
     index_or_count: int
+
+    @staticmethod
+    def from_int(as_int: int) -> TrailerIndexOrCount:
+        if not (0 <= as_int <= 255):
+            raise ValueError("TrailerIndexOrCount integer must fit within unsigned 8-bit width")
+        return TrailerIndexOrCount(as_int & 1 > 0, as_int >> 1)
+
+    def __int__(self) -> int:
+        return int(self.is_count) | (self.index_or_count << 1)
 
 
 class RequestType(Enum):
@@ -60,14 +72,17 @@ class CommunicationError(Exception):
 
 
 class Connection:
-    PORT = 52878
+    PORT: int = 52878
+    TELEMETRY_DATA_START: int = 1 + 2
+    DATA_DEFINITION_DATA_START: int = 1 + 1
+    DEFINED_DATA_DATA_START: int = 1 + 1
 
     def __init__(self, ip: str = "127.0.0.1") -> None:
         self.addr: tuple[str, int] = ip, Connection.PORT
         self._socket: socket = socket(AddressFamily.AF_INET, SocketKind.SOCK_STREAM, IPPROTO_TCP)
         self._connected: bool = False
         self.pending_request: RequestType = RequestType.NoRequest
-        self.pending_telemetry_id: int = INVALID_TELEMETRY_ID
+        self.pending_telemetry_id: TelemetryID = TelemetryID.Invalid
         self.pending_trailer_index_or_count: TrailerIndexOrCount = TrailerIndexOrCount(False, 0)
         self.collector: Collector = Collector()
 
@@ -93,6 +108,9 @@ class Connection:
         self._ensure_connected()
         if self.pending_request == RequestType.NoRequest:
             raise CommunicationError(CommunicationResult.NoPendingRequest)
+
+        if self.collector.data_ready:
+            self.collector.reset()
         while not self.collector.error_state and not self.collector.data_ready:
             if not (recv := self._socket.recv(1)):
                 raise CommunicationError(CommunicationResult.Disconnected)
@@ -100,7 +118,7 @@ class Connection:
 
     def get_version(self) -> Version:
         self._ensure_connected()
-        self._ensure_pendinng_request(RequestType.NoRequest)
+        self._ensure_pending_request(RequestType.NoRequest)
 
         self.pending_request = RequestType.Version
         self._socket.send(encode_with_size([RequestType.Version.value]))
@@ -110,16 +128,56 @@ class Connection:
         if self.collector.next_size == 0:
             raise CommunicationError(CommunicationResult.UnknownData)
 
-        if self.collector.bytearray[0] == RequestType.ErrorResponse:
+        if self.collector.bytearray[0] == RequestType.ErrorResponse.value:
             raise CommunicationError(CommunicationResult(self.collector.bytearray[1]))
 
-        if self.collector.bytearray[0] == RequestType.Version:
+        if self.collector.bytearray[0] != RequestType.Version.value:
             raise CommunicationError(CommunicationResult.ReceivedOtherResponse)
 
         if self.collector.next_size != 1 + 4:
             raise CommunicationError(CommunicationResult.UnknownData)
 
         return Version.from_int(int.from_bytes(self.collector.bytearray[1:4], "little"))
+
+    def send_request_for(self, telemetry_id: TelemetryID, trailer_index_or_count: TrailerIndexOrCount | None = None) -> None:
+        self._ensure_connected()
+        self._ensure_pending_request(RequestType.NoRequest)
+        trailer_index_or_count = trailer_index_or_count or TrailerIndexOrCount(False, 0)
+        self._socket.send(encode_with_size([
+            RequestType.TelemetryID.value,
+            telemetry_id.value,
+            int(trailer_index_or_count)
+        ]))
+        self.pending_request = RequestType.TelemetryID
+        self.pending_telemetry_id = telemetry_id
+        self.pending_trailer_index_or_count = trailer_index_or_count
+
+    def receive_for_request(self, telemetry_id: TelemetryID, trailer_index_or_count: TrailerIndexOrCount | None = None) -> None:
+        self._ensure_connected()
+        self._ensure_pending_request(RequestType.TelemetryID)
+        trailer_index_or_count = trailer_index_or_count or TrailerIndexOrCount(False, 0)
+
+        if self.pending_telemetry_id != telemetry_id:
+            raise CommunicationError(CommunicationResult.OtherTelemetryIDPending)
+        if self.pending_trailer_index_or_count != trailer_index_or_count:
+            raise CommunicationError(CommunicationResult.OtherTrailerIndexRequestPending)
+
+        self.receive_all()
+        self.pending_request = RequestType.NoRequest
+        if self.collector.next_size < 2:
+            raise CommunicationError(CommunicationResult.UnknownData)
+
+        if self.collector.bytearray[0] == RequestType.ErrorResponse.value:
+            raise CommunicationError(CommunicationResult(self.collector.bytearray[1]))
+
+        if self.collector.bytearray[0] != RequestType.TelemetryID.value:
+            raise CommunicationError(CommunicationResult.ReceivedOtherResponse)
+
+        if self.collector.bytearray[1] != telemetry_id.value:
+            raise CommunicationError(CommunicationResult.ReceivedOtherTelemetry)
+
+        if TrailerIndexOrCount.from_int(self.collector.bytearray[2]) != trailer_index_or_count:
+            raise CommunicationError(CommunicationResult.ReceivedOtherTrailerIndex)
 
     def disconnect(self) -> None:
         if not self._connected:
@@ -131,7 +189,7 @@ class Connection:
         if not self.connected:
             raise CommunicationError(CommunicationResult.NotConnected)
 
-    def _ensure_pendinng_request(self, request_type: RequestType) -> None:
+    def _ensure_pending_request(self, request_type: RequestType) -> None:
         if self.pending_request != request_type:
             raise CommunicationError(CommunicationResult.OtherRequestPending)
 
@@ -139,8 +197,23 @@ class Connection:
 def main() -> None:
     connection: Connection = Connection()
     connection.connect()
-    print(VERSION)
-    print(str(connection.get_version()))
+    print(f"Version: {VERSION}, Server Version: {connection.get_version()}")
+
+    start_time: float = time()
+    duration: float = 5 * 60
+    while time() - start_time < duration:
+        connection.send_request_for(TelemetryID.ChannelPaused)
+        connection.receive_for_request(TelemetryID.ChannelPaused)
+        _, paused = unpack_from("=??", connection.collector.bytearray, Connection.TELEMETRY_DATA_START)
+        if paused:
+            print("<paused>")
+        else:
+            connection.send_request_for(TelemetryID.ChannelGameTime)
+            connection.receive_for_request(TelemetryID.ChannelGameTime)
+            initialized, game_time = unpack_from("=?I", connection.collector.bytearray, Connection.TELEMETRY_DATA_START)
+            print(game_time if initialized else ".")
+        sleep(0.1)
+
     connection.disconnect()
 
 
